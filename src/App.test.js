44a503ae887c11e1112c1411dfa00275aa46f0f9
Naveed Anjum <administrator@Naveeds-MacBook-Pro.local>
2021-06-17 18:39:41 +0530

    message

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import regeneratorRuntime from '@babel/plugin-transform-regenerator';
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
